ed97cdd51c48d2d367ce0b3658482f69e77d9fd9
Vince <vhorvath2010@gmail.com>
2022-07-10 23:08:22 -0400

    began creating file for single match prediction

---
+++ predict.py
@@ -0,0 +1,6 @@
+# Script to run a prediction on a single match
+t1_rank = int(input("Enter the first team's rank: "))
+t2_rank = int(input("Enter the second team's rank: "))
+h2h = int(input("Enter the h2h ranking: "))
+t1_form = int(input("Enter the first team's form: "))
+t2_form = int(input("Enter the second team's form: "))
